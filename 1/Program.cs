30e4c7c1a378debfcab7a5e2e207e33d0ba6b45f
Andrey Savaskov <ultrazokk@gmail.com>
2023-12-06 12:40:19 +0300

    Выполнено первое задание

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] array2D = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+Console.WriteLine("Введите индексы элемента в массиве: ");
+
+Console.Write("Индекс строки: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Индекс столбца: ");
+int col = int.Parse(Console.ReadLine());
+
+int numRows = array2D.GetLength(0);
+int numCols = array2D.GetLength(1);
+
+if (row >= 0 && row < numRows && col >= 0 && col < numCols)
+{
+    int value = array2D[row, col];
+    Console.WriteLine($"Значение элемента: {value}");
+}
+else
+{
+    Console.WriteLine("Элемент с указанными индексами отсутствует в массиве.");
+}
